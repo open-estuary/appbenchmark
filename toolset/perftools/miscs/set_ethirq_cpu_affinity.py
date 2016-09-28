123c383f14dda4e028960b64e9fbe05a0dd6b7a4
sjtuhjh@hotmail.com <sjtuhjh@hotmail.com>
2016-09-28 10:24:33 +0800

    update script to set cpu affinity

--- toolset/perftools/miscs/set_ethirq_cpu_affinity.py
+++ toolset/perftools/miscs/set_ethirq_cpu_affinity.py
@@ -17,7 +17,7 @@ import collections
 def set_ethirq_cpu_affinity():
 
     if len(sys.argv) < 3:
-        print("Usage: set_cpu_affinity_ethirq.py <start cpu index> <end cpu index>")
+        print("Usage: set_cpu_affinity_ethirq.py <start cpu index> <end cpu index> {clear}")
         return -1
     
     cpu_start = int(sys.argv[1])
@@ -26,6 +26,10 @@ def set_ethirq_cpu_affinity():
     if cpu_start > cpu_end:
         print("The <start cpu index> should be less than <end cpu index> ")
         return -1
+
+    clear_flag = False    
+    if len(sys.argv) >= 4 and sys.argv[3] == "clear":
+        clear_flag = True
     
     #Read irq number of ethernet
     irq_file = open("/proc/interrupts")
@@ -38,14 +42,19 @@ def set_ethirq_cpu_affinity():
         device = elems[-1]
         
         #Ignoren non ethernet devices
-        #if not re.search("eth", device):
-        #    continue
+        if not re.search("eth", device):
+            continue
+
+	print("%s-%s"%(irq_num, device))
+
         try :
-            irq_num = int(irq_num[:-2])
+            irq_num = int(irq_num[:-1])
         except :
             continue
 
         ethname = device.split('-')[0]
+       
+	print("%s-%s"%(irq_num, ethname))
         if not eth_irq_dict.has_key(ethname):
             eth_irq_dict[ethname] = [irq_num]
         else :
@@ -55,17 +64,22 @@ def set_ethirq_cpu_affinity():
     cur_cpu = cpu_start
     for ethname in eth_irq_dict.keys():
         irq_num_list = eth_irq_dict[ethname]
-
-        
+ 
         cpu_hex_num = (0x1 << cur_cpu)
         print("cpu:%x"%cpu_hex_num)
 
         cpu_hex_num = str(hex(cpu_hex_num))
+	if cpu_hex_num.startswith('0x') or cpu_hex_num.startswith('0x'):
+	    cpu_hex_num = cpu_hex_num[2:]
 
         for irq_num in irq_num_list:
-            cmd_str = "echo " + str(cpu_hex_num) + " > " + "/proc/irq/"+str(irq_num)+"/smp_affinity"
+            if not clear_flag:
+                cmd_str = "echo " + str(cpu_hex_num) + " > " + "/proc/irq/"+str(irq_num)+"/smp_affinity"
+            else :
+                cmd_str = "echo ffffffff > /proc/irq/"+str(irq_num)+"/smp_affinity"
+
             print("cmd:%s"%cmd_str)
-            #os.system(cmd_str)
+            os.system(cmd_str)
         cur_cpu += 1
         if cur_cpu > cpu_end:
             cur_cpu = cpu_start
