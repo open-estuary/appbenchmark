caed913c635c399a7f21a451a9dddc776af83236
sjtuhjh@hotmail.com <sjtuhjh@hotmail.com>
2016-12-14 21:41:01 +0000

    update percona server test script

--- apps/mysql/percona_1/run_test.sh
+++ apps/mysql/percona_1/run_test.sh
@@ -4,12 +4,20 @@ default_ip="192.168.10.174"
 ip=${1}
 
 if [ $# -lt 1 ] ; then 
-    echo "Usage: ./run_test.sh {ip}"
+    echo "Usage: ./run_test.sh {ip} ${test_multi_num=1}"
     ip=${default_ip}
     echo "Use default IP:${ip}"
 fi
 
-#./run_client.sh ${ip} 
+./run_client.sh ${ip} 
 
 #./scripts/warmup.sh ${ip}
-./scripts/init_client.sh ${ip} test
+
+index=1
+while [[ ${index} -lt 20 ]] 
+do
+./scripts/init_client.sh ${ip} test ${index}
+sleep 7200
+let "index++"
+done
+
