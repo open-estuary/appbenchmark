faa05c066980555fc5c51bc27ce263b77cfc8814
sjtuhjh@hotmail.com <sjtuhjh@hotmail.com>
2016-12-27 01:54:36 +0000

    update test script

--- apps/mysql/alisql_2/run_test.sh
+++ apps/mysql/alisql_2/run_test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-./run_client.sh test 200   > ./testresult_200_1thread
-./run_client.sh test 200 4 > ./testresult_200_4thread
-./run_client.sh test 200 8 > ./testresult_200_8thread
+if [ -z "${1}" ] ; then
+    echo "Usage: ./run_client.sh {ip}"
+fi
+
+./scripts/init_client.sh ${1} test 200   > ./testresult_200_1thread
+./scripts/init_client.sh ${1} test 200 4 > ./testresult_200_4thread
+./scripts/init_client.sh ${1} test 200 8 > ./testresult_200_8thread
 
