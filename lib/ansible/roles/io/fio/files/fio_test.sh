8e4da0faa28f9be41903923e8934215063e8a7df
wanglei <seilwang@163.com>
2017-09-07 21:18:46 +0800

    modify fio test script

---
+++ lib/ansible/roles/io/fio/files/fio_test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -z "$(which fio 2>/dev/null)" ] ; then
+    echo "Please install fio firstly !"
+    exit 0
+fi
+if [ -z "$(which ifconfig 2>/dev/null)" ] ; then
+    echo "Please install net-tools firstly !"
+    yum install net-tools -y
+    exit 0
+fi
+IP=`ifconfig eth0|sed -n 2p|awk  '{ print $2 }'|tr -d 'addr:'`;
+CUR_DIR=$(cd `dirname $0`; pwd)
+if [ ! -d "${CUR_DIR}/result" ]; then
+  mkdir -p  ${CUR_DIR}/result
+fi
+IOTEST_DIR="${CUR_DIR}/result"
+pushd ${IOTEST_DIR} > /dev/null
+
+nohup fio fio.cfg  >>result_fiotest_${IP} 2>&1 & 
+
+popd > /dev/null
+
--- lib/ansible/roles/io/fio/files/run_test.sh
+++ lib/ansible/roles/io/fio/files/run_test.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 
-if [ -z "$(which sysbench 2>/dev/null)" ] ; then
+if [ -z "$(which fio 2>/dev/null)" ] ; then
     echo "Please install sysbench firstly !"
     exit 0
 fi
+if [ -z "$(which ifconfig 2>/dev/null)" ] ; then
+    echo "Please install net-tools firstly !"
+    yum install net-tools -y
+    exit 0
+fi
+
 IP=`ifconfig eth0|sed -n 2p|awk  '{ print $2 }'|tr -d 'addr:'`;
 CUR_DIR=$(cd `dirname $0`; pwd)
 if [ -d "${CUR_DIR}/result" ]; then
   rm -rf ${CUR_DIR}/result
 fi
-SLEEPTIME=305
+SLEEPTIME=6500
 pushd ${CUR_DIR} > /dev/null
-sh rndread.sh 
-sleep ${SLEEPTIME} 
-sh rndrw.sh
-sleep ${SLEEPTIME} 
-sh  rndwrite.sh
-sleep ${SLEEPTIME}
-sh seqread.sh 
-sleep ${SLEEPTIME}
-sh seqwrite.sh
-sleep ${SLEEPTIME}
+#sh rndread.sh 
+#sleep ${SLEEPTIME} 
+#sh rndrw.sh
+#sleep ${SLEEPTIME} 
+#sh  rndwrite.sh
+#sleep ${SLEEPTIME}
+#sh seqread.sh 
+#sleep ${SLEEPTIME}
+#sh seqwrite.sh
+#sleep ${SLEEPTIME}
+sh fio_test.sh
 popd > /dev/null
 
 
