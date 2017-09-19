c852cc959894b1bf7c5efa8298f2d59648b3ad27
sjtuhjh@hotmail.com <sjtuhjh@hotmail.com>
2017-09-19 15:41:41 +0800

    add solr benchmark test scripts

---
+++ apps/solr/ansible/roles/solr_benchmark_setup/files/download_solr_testscripts.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+work_dir="${1}/solr_benchmark"
+
+if [ ! -d "${work_dir}" ] ; then
+    mkdir -p ${work_dir}
+fi
+
+pushd ${work_dir} > /dev/null
+
+wget https://raw.githubusercontent.com/open-estuary/packages/master/solutions/e-commerce-springcloud-microservices/scripts/solr_benchmark/solr_benchmark.jmx
+wget https://raw.githubusercontent.com/open-estuary/packages/master/solutions/e-commerce-springcloud-microservices/scripts/solr_benchmark/solr_benchmark_test.sh
+wget https://raw.githubusercontent.com/open-estuary/packages/master/solutions/e-commerce-springcloud-microservices/scripts/solr_benchmark/solr_create_fields.sh
+wget https://raw.githubusercontent.com/open-estuary/packages/master/solutions/e-commerce-springcloud-microservices/scripts/solr_benchmark/solr_post_data.sh
+wget https://raw.githubusercontent.com/open-estuary/packages/master/solutions/e-commerce-springcloud-microservices/scripts/solr_benchmark/solr_query
+
+sudo chmod 755 solr_benchmark_test.sh
+sudo chmod 755 solr_create_fields.sh
+sudo chmod 755 solr_post_data.sh
+
+popd > /dev/null
+
+
+
