973c3a80e0a79486554ffe17e9b36f4b9bdba36b
sjtuhjh@hotmail.com <sjtuhjh@hotmail.com>
2017-09-19 17:00:39 +0800

    update solr test scripts

--- apps/solr/ansible/roles/solr_benchmark_setup/files/download_solr_testscripts.sh
+++ apps/solr/ansible/roles/solr_benchmark_setup/files/download_solr_testscripts.sh
@@ -8,11 +8,11 @@ fi
 
 pushd ${work_dir} > /dev/null
 
-wget https://raw.githubusercontent.com/open-estuary/packages/master/solutions/e-commerce-springcloud-microservices/scripts/solr_benchmark/solr_benchmark.jmx
-wget https://raw.githubusercontent.com/open-estuary/packages/master/solutions/e-commerce-springcloud-microservices/scripts/solr_benchmark/solr_benchmark_test.sh
-wget https://raw.githubusercontent.com/open-estuary/packages/master/solutions/e-commerce-springcloud-microservices/scripts/solr_benchmark/solr_create_fields.sh
-wget https://raw.githubusercontent.com/open-estuary/packages/master/solutions/e-commerce-springcloud-microservices/scripts/solr_benchmark/solr_post_data.sh
-wget https://raw.githubusercontent.com/open-estuary/packages/master/solutions/e-commerce-springcloud-microservices/scripts/solr_benchmark/solr_query
+wget -O solr_benchmark.jmx  https://raw.githubusercontent.com/open-estuary/packages/master/solutions/e-commerce-springcloud-microservices/scripts/solr_benchmark/solr_benchmark.jmx
+wget -O solr_benchmark_test.sh https://raw.githubusercontent.com/open-estuary/packages/master/solutions/e-commerce-springcloud-microservices/scripts/solr_benchmark/solr_benchmark_test.sh
+wget -O solr_create_fields.sh https://raw.githubusercontent.com/open-estuary/packages/master/solutions/e-commerce-springcloud-microservices/scripts/solr_benchmark/solr_create_fields.sh
+wget -O solr_post_data.sh https://raw.githubusercontent.com/open-estuary/packages/master/solutions/e-commerce-springcloud-microservices/scripts/solr_benchmark/solr_post_data.sh
+wget -O solr_query https://raw.githubusercontent.com/open-estuary/packages/master/solutions/e-commerce-springcloud-microservices/scripts/solr_benchmark/solr_query
 
 sudo chmod 755 solr_benchmark_test.sh
 sudo chmod 755 solr_create_fields.sh
