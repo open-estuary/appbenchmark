51e1b03ee829c506599c4e7a8311bb5c118f5c2a
wangyu <wy200885@163.com>
2017-10-25 16:44:29 +0800

    update run_twemproxy.sh script

--- apps/redis/redis_test1/run_twemproxy.sh
+++ apps/redis/redis_test1/run_twemproxy.sh
@@ -32,7 +32,7 @@ echo "begin to run twemproxy"
 while (( ${start_cpu_num} <= ${end_cpu_num} ))
 do
     twemproxy_portid=`expr 22200 + ${start_cpu_num}`
-    taskset -c ${start_cpu_num} ${TWEMPROXY_INSTALL_DIR}/twemproxy -d -c ${TWEMPROXY_CFG_DIR}/nutcracker_${start_cpu_num}.yml -v 4 -s ${twemproxy_portid} -m 1024 -o twemproxy_${start_cpu_num}.log
+    taskset -c ${start_cpu_num} ${TWEMPROXY_INSTALL_DIR}/twemproxy -d -c ${TWEMPROXY_CFG_DIR}/nutcracker_${start_cpu_num}.yml -v 4 -s ${twemproxy_portid} -m 1024 
     # taskset -c 34 /usr/sbin/twemproxy -d -c /usr/sbin/conf/nutcracker_34.yml -v 4 -m 1024 -s 22234
     
     let "start_cpu_num++"
