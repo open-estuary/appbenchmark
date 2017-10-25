d072b3944c100246ddbd4267e68b55c9f50188ea
wangyu <wy200885@163.com>
2017-10-25 16:41:51 +0800

    update redis and twemproxy configuration files

--- apps/redis/redis_test1/run_twemproxy.sh
+++ apps/redis/redis_test1/run_twemproxy.sh
@@ -1,16 +1,43 @@
 #!/bin/bash
 
+if [ $# -lt 2 ]; then
+    echo "Usage: run_twemproxy.sh <start_cpu_num> <end_cpu_num>"
+    exit 0
+fi
+
+start_cpu_num=${1}
+end_cpu_num=${2}
+
+APP_CUR_DIR=$(cd `dirname $0` ; pwd)
+
+TWEMPROXY_INSTALL_DIR=/usr/sbin
+TWEMPROXY_CFG_DIR=${TWEMPROXY_INSTALL_DIR}/conf
+
+############################
+# Notes:
+# Copy twemproxy
+#############################
+mkdir -p ${TWEMPROXY_INSTALL_DIR}/conf
+cp ${APP_CUR_DIR}/config/nutcracker* ${TWEMPROXY_INSTALL_DIR}/conf/
+
 ##############################
 # Notes:
 # run twemproxy
 #############################
 
-echo "begin to run twemproxy"
-
-TWEMPROXY_INSTALL_DIR=/usr/local/sbin
-
 pushd ${TWEMPROXY_INSTALL_DIR} > /dev/null
 
-taskset -c 30 ${TWEMPROXY_INSTALL_DIR}/nutcracker -d -c conf/nutcracker.yml -v 4 -m 1024 -s 22222
+echo "begin to run twemproxy"
+
+while (( ${start_cpu_num} <= ${end_cpu_num} ))
+do
+    twemproxy_portid=`expr 22200 + ${start_cpu_num}`
+    taskset -c ${start_cpu_num} ${TWEMPROXY_INSTALL_DIR}/twemproxy -d -c ${TWEMPROXY_CFG_DIR}/nutcracker_${start_cpu_num}.yml -v 4 -s ${twemproxy_portid} -m 1024 -o twemproxy_${start_cpu_num}.log
+    # taskset -c 34 /usr/sbin/twemproxy -d -c /usr/sbin/conf/nutcracker_34.yml -v 4 -m 1024 -s 22234
+    
+    let "start_cpu_num++"
+done
+
+echo "twemproxy is running"
 
 popd > /dev/null
