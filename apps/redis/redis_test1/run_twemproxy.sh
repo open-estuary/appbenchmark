9df78cfda4fbd24abdf4d78f76b3f84b53062ea7
wangyu <wy200885@163.com>
2017-10-25 16:00:31 +0800

    update script to install twemproxy with ansible

--- apps/redis/redis_test1/run_twemproxy.sh
+++ apps/redis/redis_test1/run_twemproxy.sh
@@ -11,6 +11,6 @@ TWEMPROXY_INSTALL_DIR=/usr/local/sbin
 
 pushd ${TWEMPROXY_INSTALL_DIR} > /dev/null
 
-taskset -c 30 ${TWEMPROXY_INSTALL_DIR}/nutcracker conf/nutcracker.yml -v 4 -m 1024
+taskset -c 30 ${TWEMPROXY_INSTALL_DIR}/nutcracker -d -c conf/nutcracker.yml -v 4 -m 1024 -s 22222
 
 popd > /dev/null
