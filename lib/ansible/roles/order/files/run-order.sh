e07262f6ac73cae5b0504fd58aff0679100bf6eb
sjtuhjh@hotmail.com <sjtuhjh@hotmail.com>
2017-10-26 17:25:20 +0800

    modify order

--- lib/ansible/roles/order/files/run-order.sh
+++ lib/ansible/roles/order/files/run-order.sh
@@ -5,8 +5,7 @@ FILENAME=application-order.yml
 TARNAME=micro-service-1.0-order.tar.gz
 JARNAME=order.jar
 
-IP_ARR=`ip addr | grep "eth0" | grep "inet" | awk '{ print $2}'`
-IP=${IP_ARR%/*}
+CPU_NUMBER=${1}
 
 sudo wget -O /etc/yum.repos.d/estuary.repo https://raw.githubusercontent.com/open-estuary/distro-repo/master/estuaryftp.repo     
 sudo chmod +r /etc/yum.repos.d/estuary.repo
@@ -21,9 +20,4 @@ mv ${CUR_DIR}/order/target/order-0.0.1-SNAPSHOT.jar ${CUR_DIR}/order/${JARNAME}
 rm -rf ${CUR_DIR}/order/target ${CUR_DIR}/order/${TARNAME}
 popd > /dev/null
 
-if [ "$IP" = "192.168.1.115" ] ; then
-	nohup taskset -c 48-63 java -jar -Dspring.config.location=${CUR_DIR}/${FILENAME} ${CUR_DIR}/order/${JARNAME} 2>&1 &
-fi
-if [ "$IP" = "192.168.1.120" ] ; then
-	nohup taskset -c 46-63 java -jar -Dspring.config.location=${CUR_DIR}/${FILENAME} ${CUR_DIR}/order/${JARNAME} 2>&1 &
-fi
+nohup taskset -c ${CPU_NUMBER} java -jar -Dspring.config.location=${CUR_DIR}/${FILENAME} ${CUR_DIR}/order/${JARNAME} 2>&1 &
