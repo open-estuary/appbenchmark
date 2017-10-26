e0bed5e3cf31fbefe7894cb72830560beb2de2c9
sjtuhjh@hotmail.com <sjtuhjh@hotmail.com>
2017-10-26 20:29:38 +0800

    modify order cart search service taskset

--- lib/ansible/roles/search/files/run-search.sh
+++ lib/ansible/roles/search/files/run-search.sh
@@ -5,8 +5,7 @@ FILENAME=application-search.yml
 TARNAME=micro-service-1.0-search.tar.gz
 JARNAME=search.jar
 
-IP_ARR=`ip addr | grep "eth0" | grep "inet" | awk '{ print $2}'`
-IP=${IP_ARR%/*}
+CPU_NUMBER=${1}
 
 sudo wget -O /etc/yum.repos.d/estuary.repo https://raw.githubusercontent.com/open-estuary/distro-repo/master/estuaryftp.repo     
 sudo chmod +r /etc/yum.repos.d/estuary.repo               
@@ -21,10 +20,4 @@ mv ${CUR_DIR}/search/target/htsat-search.jar ${CUR_DIR}/search/${JARNAME}
 rm -rf ${CUR_DIR}/search/target ${CUR_DIR}/search/${TARNAME}
 popd > /dev/null
 
-if [ "$IP" = "192.168.1.175" ] ; then
-	nohup taskset -c 48-63 java -jar -Dspring.config.location=${CUR_DIR}/${FILENAME} ${CUR_DIR}/search/${JARNAME} >/dev/null 2>&1 &
-fi
-
-if [ "$IP" = "192.168.1.160" ] ; then
-	nohup taskset -c 34-46 java -jar -Dspring.config.location=${CUR_DIR}/${FILENAME} ${CUR_DIR}/search/${JARNAME} >/dev/null 2>&1 &
-fi
+nohup taskset -c ${CPU_NUMBER} java -jar -Dspring.config.location=${CUR_DIR}/${FILENAME} ${CUR_DIR}/search/${JARNAME} >/dev/null 2>&1 &
