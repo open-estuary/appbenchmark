ae8f7ad5d4e390dedc4b529a47e1ee92ee41eb1f
y00369814 <yulijie@huawei.com>
2017-11-07 13:45:48 +0800

    [Changelog]: delete calico [Author]: y00369814 [Date]: 2017/11/7

--- lib/ansible/roles/docker_net/calico/templates/config_docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "{" > /etc/docker/daemon.json
-echo -e "  \"cluster-store\":\"etcd://{{ Etcd_Server }}:2379\" " >> /etc/docker/daemon.json
-echo "}" >> /etc/docker/daemon.json
-
-systemctl restart docker.service
-
