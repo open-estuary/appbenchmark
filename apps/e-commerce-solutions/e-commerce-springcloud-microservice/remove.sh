4ec403ae0b2f2d7932244840bea9901e4ccd22b6
sjtuhjh@hotmail.com <sjtuhjh@hotmail.com>
2017-11-16 09:47:08 +0800

    update e-commerce setup scripts

--- apps/e-commerce-solutions/e-commerce-springcloud-microservice/remove.sh
+++ apps/e-commerce-solutions/e-commerce-springcloud-microservice/remove.sh
@@ -4,7 +4,7 @@ CURDIR=$(cd `dirname $0`; pwd)
 
 pushd ${CURDIR}/ansible > /dev/null
 
-ansible-playbook -i hosts site_remove.yml  --user=estuaryapp --extra-vars "ansible_sudo_pass=estuaryapp"  --extra-vars "packages_state=absent" --extra-vars "rpm_packages_list=Percona-Server-server-56,twemproxy,nginx,solr,micro-service-api,micro-service-cart,micro-service-discovery,micro-service-order,micro-service-search,redis,mysql"
+ansible-playbook -i hosts site_remove.yml  --user=estuaryapp  --extra-vars "repo_metadata_clean=true" --extra-vars "ansible_sudo_pass=estuaryapp"  --extra-vars "packages_state=absent" --extra-vars "rpm_packages_list=Percona-Server-server-56,twemproxy,nginx,solr,micro-service-api,micro-service-cart,micro-service-discovery,micro-service-order,micro-service-search,redis,mysql"
 
 popd > /dev/null
 
